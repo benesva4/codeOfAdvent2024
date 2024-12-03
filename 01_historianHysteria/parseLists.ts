d31782f7e71d9ef85c781ff46aad83019814c4b3
Václav Beneš <pan.vaclav.benes@gmail.com>
2024-12-04 00:16:52 +0100

    fmt

--- 01_historianHysteria/parseLists.ts
+++ 01_historianHysteria/parseLists.ts
@@ -1,14 +1,14 @@
 export function parseLists(input: string) {
-  const rows = input.trimEnd().split("\n");
+  const rows = input.trimEnd().split('\n')
 
-  const leftList: number[] = [];
-  const rightList: number[] = [];
+  const leftList: number[] = []
+  const rightList: number[] = []
 
   rows.forEach((row) => {
-    const [leftId, rightId] = row.split("   ");
-    leftList.push(Number(leftId));
-    rightList.push(Number(rightId));
-  });
+    const [leftId, rightId] = row.split('   ')
+    leftList.push(Number(leftId))
+    rightList.push(Number(rightId))
+  })
 
-  return [leftList, rightList];
+  return [leftList, rightList]
 }
